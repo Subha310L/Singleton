3906c87a84a50a205c91f204155a4dfbf3cc09bf
Subha Lakshmi Lakshmanaperumal <subhalakshmi.lakshmanaperumal@gyansys.com>
2022-12-19 11:38:18 +0530

    Singleton

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton frombank = Singleton.GetInstance;
+            frombank.PrintDetails("From Bank ");
+            Singleton fromCustomer = Singleton.GetInstance;
+            fromCustomer.PrintDetails("From customer");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// https://dotnettutorials.net/lesson/singleton-design-pattern/ -- read
---
+++ Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singleton
+{
+
+    // class singleton is seales - cls cannot be inherited and obj instantiation
+    //class created with a private constructor
+    
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        //only one instance of the cls is created based on the null condition
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
